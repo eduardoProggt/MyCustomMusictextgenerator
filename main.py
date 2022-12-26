33e613e68e2459f2f79ebb7eb8378efe1f4a2d44
Herodes <roadslocker@gmail.com>
2022-12-26 21:22:23 +0100

    Update main.py
    Verfeinerungen und Ideen

--- main.py
+++ main.py
@@ -13,17 +13,21 @@ def addline():
 	text = ""
 	randint_for_line_type = random.randint(0, 22)
 	if randint_for_line_type > 20:
-		text+= "Jeder "+getRandomElement(lineData.substantive_maennlich)+" will mich dissen \njeder "+getRandomElement(lineData.substantive_maennlich)+" kann sich verpissen \n"+getRandomElement(lineData.substantive_mehrzahl)+" haben schlechtes gewissen"
+		substantive = getRandomElementsExclusive(lineData.substantive_maennlich,2)
+		text+= "Jeder "+ substantive[0] +" will mich dissen \nJeder "+substantive[1]+" kann sich verpissen \n"+getRandomElement(lineData.substantive_mehrzahl)+" haben schlechtes Gewissen"
 	elif randint_for_line_type > 15:
 		text+= getRandomElement(line_standalones)
 	elif randint_for_line_type > 1:
 		text+= getRandomElement(line_line_beginnings) + getRandomElement(line_endings)
 	elif randint_for_line_type > 0:
-		text+= "Was soll ich tun \nSoll ich "+getRandomElement(line_verbs)+" oder ruhn?"
+		text+= "Was soll ich tun? \nSoll ich "+getRandomElement(line_verbs)+" oder ruhn?"
 	else:
 		verb = getRandomElementsExclusive(line_verbs,3)
 		text+= verb[0] +", " +verb[1] +", "+verb[2] +" und zählen die Scheine" 
 	return text +" \n"
+	
+	#Mehrzahl haben ne Weiblich
+	#'blamage, visage'
 
 def getRandomElement(line):
 	return line[random.randint(0, len(line) - 1)]

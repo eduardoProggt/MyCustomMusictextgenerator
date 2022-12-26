c58c5cfdd27982286130d60e30c09385c481eeda
Herodes <roadslocker@gmail.com>
2022-12-26 20:00:25 +0100

    Update main.py
    Refactoring + debugging

--- main.py
+++ main.py
@@ -7,23 +7,30 @@ line_begins= ["Alle Leute sagen zu mir",
 "Auch wenn du denkst",
 "Ohne nice"]
 
-line_ends = [" bin so genial.", " bist so abgezeckt", " du wohnst in den Knast"]
+line_ends = [" bin so genial.", " bist so abgezeckt", " du wohnst in den Knast", " geh widder Heim"]
 
 line_standalone = [
-"Zeig' ich dir den Mittelfinger"
+"Zeig\' ich dir den Mittelfinger",
 "Bleib ich wie ich bin",
 "Mach es ausm Kopf",
 ]
 
 text = ""
 
-def addline(text):
+def addline():
+	text = ""
 	if random.randint(1, 3) == 3:
-		text+= line_standalone[random.randint(1, len(line_standalone)-1)]
+		text+= getRandomElement(line_standalone)
 	else:
-		text+= line_begins[random.randint(1, len(line_begins)-1)] + line_ends[random.randint(1, len(line_ends)-1)]
-	return text +"\n"
+		text+= getRandomElement(line_begins) + getRandomElement(line_ends)
+
+	return text +" \n"
+
+def getRandomElement(line):
+	return line[random.randint(0, len(line) - 1)]
+
+
 for i in range(1,8):
-	text = addline(text)
+	text += addline()
 
 print(text)

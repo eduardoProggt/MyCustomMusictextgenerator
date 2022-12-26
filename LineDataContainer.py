0e17713eef1041383c0bcc57a1ecf591488d8a66
Herodes <roadslocker@gmail.com>
2022-12-26 22:08:45 +0100

    DB komplett angebunden

--- LineDataContainer.py
+++ LineDataContainer.py
@@ -7,25 +7,9 @@ class LineDataContainer():
 		json_file = open(path_to_json)
 		data = json.load(json_file)
 
-		self.line_beginnings= ["Alle Leute sagen zu mir:",
-		"Es ist nicht so leicht",
-		"Kann es aber sein",
-		"Es ist mir egal",
-		"Auch wenn du denkst",
-		"Ohne nice",
-		"Gebe niemals auf",
-		"Mach es ausm Kopf",
-		"Weiß ich ganz genau"]
-
-		self.line_endings = [" bin so genial.", " bist so abgezeckt", " du wohnst in den Knast", " geh widder Heim", " es ist der pure Neid", " lass es aber sein", " ihr werdet schon sehn"]
-
-		self.line_standalones = [
-		"Zeig\' ich dir den Mittelfinger",
-		"Bleib ich wie ich bin",
-		"Bin der King mit dem dicken Ding",
-		"Es ist wie ein Stich in Herz"
-		]
-
+		self.line_beginnings = data["Line_Beginnings"]
+		self.line_endings = data["Line_Endings"]
+		self.line_standalones = data["Line_Standalone"]
 		self.line_verbs = data['Verben']
 		self.substantive_maennlich = data['Substantiv_maennlich']
 		self.substantive_mehrzahl = data["Substantiv_mehrzahl"]

e26633fabfb3bfdf8dd14e00f0f575a20e7b8eea
Herodes <roadslocker@gmail.com>
2023-01-14 12:22:25 +0100

    Last_linecount -> linecount
    Umbenannt, weil es sonst keinen Sinn ergibt

--- server.py
+++ server.py
@@ -14,20 +14,23 @@ app = Flask(__name__)
 @app.route('/')
 def index():
 	print(request)
-	return render_template('index.html',last_lineCount = 8,last_topic = "")
+	print(request.remote_addr, "hat sich raufgeschaltne")
+	return render_template('index.html',lineCount = 8,last_topic = "")
 
 @app.route('/handle_data', methods=['POST'])
 def handle_data():
 	topic = request.form['topic']
 	print("topic: "+topic)
 	lineCount = request.form['lines']
+	#Das wesentliche:
 	writer = Textwriter(topic,int(lineCount))
 	text = writer.writeText()
-	return render_template('index.html',result=text, last_lineCount = lineCount, last_topic = topic)
+	#
+	return render_template('index.html',result=text, lineCount = lineCount, last_topic = topic)
 
 @app.route('/index.css')
 def css():#
 	return send_from_directory("templates", "index.css")
 
 if __name__ == '__main__':
-	serve(app, port=50100, threads=1)
+	serve(app,port=50100, threads=2,url_scheme='https')

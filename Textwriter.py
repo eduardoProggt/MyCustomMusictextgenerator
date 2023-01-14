924a97c5a9d5ed44d018dfe675e09c32cc96944c
Herodes <roadslocker@gmail.com>
2023-01-14 12:23:34 +0100

    10 Themenlines
    ... und noch 2 Ideen.

--- Textwriter.py
+++ Textwriter.py
@@ -1,7 +1,7 @@
 import random
 from LineDataContainer import LineDataContainer
 from WordAssociator import WordAssociator
-from WordConjugator import WordConjugator
+import WordConjugator as wc
 
 from copy import deepcopy
 
@@ -20,7 +20,7 @@ def getRandomElementsExclusive(line,noOfLines):
 class Textwriter:
 
 	def __init__(self, topic = "", lineCount = 8):
-		self.topic = str(topic)
+		self.topic = str(topic).capitalize()
 		self.lineCount = int(lineCount)
 		self.text = ""
 		self.lineData = LineDataContainer('MCMdb.json')
@@ -28,18 +28,19 @@ class Textwriter:
 	def writeText(self):
 
 		withTopic = (not self.topic == "")
+		if withTopic:
+			wa = WordAssociator(self.topic)
+
 		while self.lineCount > 0:
-			if withTopic and random.randint(0, 15) == 15:
-				self.addTopicLine()
+			if withTopic and random.randint(0, 4) == 4:
+				self.addTopicLine(wa)
 			else:
 				self.addRandomLine()
-		print(self.text)
+
 		return self.text
 
 	def addRandomLine(self):
 		
-		lineData = self.lineData
-
 		randomMap = {
 			29: self.addLineHastSpastKnast,
 			27: self.addLineVorLachenBepisst,
@@ -56,7 +57,7 @@ class Textwriter:
 		while not randint_for_line_type in randomMap:
 			randint_for_line_type +=1
 
-		randomMap[randint_for_line_type](lineData)
+		randomMap[randint_for_line_type](self.lineData)
 
 
 	def addLineHastSpastKnast(self,lineData):
@@ -99,23 +100,61 @@ class Textwriter:
 		self.addLine("Soll ich "+getRandomElement(lineData.line_verbs)+" oder ruhn?")
 
 	def addLineZaehleDieScheine(self,lineData):
-		text = ""
+		
 		verb = getRandomElementsExclusive(lineData.line_verbs,3)
-		text+= verb[0].capitalize() +", " +verb[1] +", "+verb[2] +" und zählen die Scheine" 
+		text = verb[0].capitalize() +", " +verb[1] +", "+verb[2] +" und zählen die Scheine" 
 		self.addLine(text)
 
-	def addTopicLine(self):
-		text = ""
-		wa = WordAssociator(self.topic)
-		wc = WordConjugator()
-		#cb = ChatBot()
-		#text = cb.generate_response("Schreibe eine Zeile mit den Worten '"+getRandomElement(wa.nouns)+"', '"+getRandomElement(wa.verbs)+"' und '"+getRandomElement(wa.adjectives)+"'")
+	def addTopicLine(self,wa):
 		
-		if len(wa.substantive) == 0: # Wenn keine Assoziationen gefunden wurden.
-			self.addLine("Alle Rapper sagen "+self.topic+" bin so gemein")
+		randomMap = {
+			9: self.addLineInderEckeSitzt,#
+			8: self.addLineSoGenial,#
+			7: self.addLineAgalObXYoderZ,#
+			6: self.addLineJederMachtEinenAuf,#	
+			5: self.addLineObXYOderNicht,#
+			4: self.addLineWasIstDennLosMoos,#		
+			3: self.addLineEgalObSieLachen,#		
+			2: self.addLineLassEsSeinSoGemein,#	
+			1 : self.addLineEsBrauchtKeine,#		
+			0 : self.addLineAlleRapperSagen,#
+		}
+
+		#Die Untersten Keys haben Zeilen die auch ohne Assoziationen gehen
+		randint_for_line_type = random.randint(0, 9) if len(wa.nouns) > 0 else random.randint(0,4)
+
+		randomMap[randint_for_line_type](wa)
+
+	def addLineInderEckeSitzt(self,wa):
+		self.addLine("In der Ecke Sitzt "+wc.addArticle(getRandomElement(wa.nouns))+", "+wc.getPlural(getRandomElement(wa.nouns)) +" sollten sich schämen.")
+	def addLineSoGenial(self, wa):
+		self.addLine(wc.getPlural(getRandomElement(wa.getNouns()))+" "+getRandomElement(wa.verbs) +" so genial "+ getRandomElement(self.lineData.line_endings))
+	def addLineAgalObXYoderZ(self,wa):
+		topicNouns = getRandomElementsExclusive(wa.nouns,3)
+		self.addLine("Egal, ob "+wc.getPlural(topicNouns[0])+", "+topicNouns[1]+" oder "+topicNouns[2])
+	def addLineJederMachtEinenAuf(self,wa):
+		self.addLine("Jeder macht einen auf "+getRandomElement(wa.verbs))
+	def addLineObXYOderNicht(self,wa):
+		self.addLine("Ob "+wc.getPlural(getRandomElement(wa.getNouns()))+" "+getRandomElement(wa.verbs)+" oder nicht")
+
+	def addLineWasIstDennLosMoos(self,wa):
+		self.addLine("Was ist denn los? seid ihr "+wc.getPlural(getRandomElement(wa.getNouns()))+" oder Moos?")
+	def addLineEgalObSieLachen(self,wa):
+		self.addLine("Es ist mir egal, ob die " + wc.getPlural(getRandomElement(wa.getNouns())) + " über mich lachen")
+	def addLineLassEsSeinSoGemein(self,wa):
+		self.addLine(getRandomElement(self.lineData.line_beginnings)+" lass es aber sein") 
+		self.addLine("Guck die ganzen "+wc.getPlural(getRandomElement(wa.getNouns()))+" sind so gemein") 
+	def addLineEsBrauchtKeine(self, wa):
+		if self.lineCount < 2:
 			return
-		self.addLine("In der Ecke Sitzt "+wc.addArticle(getRandomElement(wa.substantive))+", "+wc.getPlural(getRandomElement(wa.substantive)) +" sollten sich schämen.")
-		# es braucht keine x auf der Welt \n dass es y auch gefällt 
+		self.addLine("Es braucht keine "+wc.getPlural(getRandomElement(wa.getNouns()))+" auf der Welt")
+		self.addLine("Dass es "+wc.getPlural(getRandomElement(wa.getNouns()))+" auch gefällt") 
+	def addLineAlleRapperSagen(self,wa):
+		self.addLine("Alle Rapper sagen "+wa.word+", bin so gemein")
+	# Weitere Ideen:
+		# alle sind die <Mehrzahl> mehr aber nein
+		# Alle <Mehrzahl> <verb> auf der Straße
+
 	def addLine(self,text):
 		if self.lineCount <=0:
 			return
